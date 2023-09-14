b92e1ba9eb8d9a311b1a11ca038ed2cea1301f84
TatsuroIde <Orust4t@gmail.com>
2023-09-14 18:45:46 +0900

    add items controller and rspec for create

---
+++ rails-training/app/controllers/v1/items_controller.rb
@@ -0,0 +1,16 @@
+class V1::ItemsController < ApplicationController
+  def create
+    item = Item.new(item_params)
+    if item.save
+      render json: { status: 'success', data: item }, status: :ok
+    else
+      render json: { status: 'error', data: item.errors }, status: :unprocessable_entity
+    end
+  end
+end
+
+private
+
+def item_params
+  params.require(:item).permit(:name, :description, :price)
+end
--- rails-training/spec/requests/items_spec.rb
+++ rails-training/spec/requests/items_spec.rb
@@ -1,17 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe "Items", type: :request do
+RSpec.describe "V1::Items", type: :request do  # V1::Items に変更
   describe "GET /items" do
     it "works! (now write some real specs)" do
-      get items_index_path
+      get v1_items_path  # items_index_path を v1_items_path に変更
       expect(response).to have_http_status(200)
     end
   end
 
   describe "POST /items" do
     it "create an item" do
-      post "/items", params: { item: { name: "Example Item", description: "Example Description", price: 100 } }
+      post v1_items_path, params: { item: { name: "Example Item", description: "Example Description", price: 100 } }
       expect(response).to have_http_status(200)
+
+      json = JSON.parse(response.body)
+      expect(json["status"]).to eq('success')
     end
   end
 end
