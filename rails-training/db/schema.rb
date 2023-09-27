779526cf161a63184b6cdc29efc93936e2d28a2c
TatsuroIde <Orust4t@gmail.com>
2023-09-27 18:51:11 +0900

    gen user model inherited ActiveRecord

--- rails-training/db/schema.rb
+++ rails-training/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_20_093018) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_27_093529) do
   create_table "categories", charset: "utf8", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -38,6 +38,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_20_093018) do
 
   create_table "tags", charset: "utf8", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", charset: "utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
