e77036fe8059b75a125de666c62a5e19a2051569
TatsuroIde <Orust4t@gmail.com>
2023-12-26 23:31:33 +0900

    try some each

---
+++ ruby/each_with_index.rb
@@ -0,0 +1,24 @@
+nums = [1, 2, 3]
+
+nums.each_with_index do |num, i|
+    p "(i, num): (#{i}, #{num})"
+end
+
+# "(i, num): (0, 1)"
+# "(i, num): (1, 2)"
+# "(i, num): (2, 3)"
+
+nums.each.with_index(1) do |num, i|
+    p "(i, num): (#{i}, #{num})"
+end
+
+# "(i, num): (1, 1)"
+# "(i, num): (2, 2)"
+# "(i, num): (3, 3)"
+
+nums[1..2].each_with_index do |num, i|
+    p "(i, num): (#{i}, #{num})"
+end
+
+# "(i, num): (0, 2)"
+# "(i, num): (1, 3)"
