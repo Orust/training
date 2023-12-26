4d815f40077e9d93776115a7726c19a6580e05d2
TatsuroIde <Orust4t@gmail.com>
2023-12-27 01:02:04 +0900

    additional case

--- ruby/each_with_index.rb
+++ ruby/each_with_index.rb
@@ -22,3 +22,51 @@ end
 
 # "(i, num): (0, 2)"
 # "(i, num): (1, 3)"
+
+nums[1..1].each_with_index do |num, i|
+    p "(i, num): (#{i}, #{num})"
+end
+
+# "(i, num): (0, 2)"
+
+nums[1, 2].each_with_index do |num, i|
+    p "(i, num): (#{i}, #{num})"
+end
+
+# "(i, num): (0, 2)"
+# "(i, num): (1, 3)"
+
+nums[1, 1].each_with_index do |num, i|
+    p "(i, num): (#{i}, #{num})"
+end
+
+# "(i, num): (0, 2)"
+
+nums[0, 3].each_with_index do |num, i|
+    p "(i, num): (#{i}, #{num})"
+end
+
+# "(i, num): (0, 1)"
+# "(i, num): (1, 2)"
+# "(i, num): (2, 3)"
+
+nums[0, 2].each.with_index(1) do |num, i|
+    p "(i, num): (#{i}, #{num})"
+end
+
+# "(i, num): (1, 1)"
+# "(i, num): (2, 2)"
+
+nums[1, 2].each.with_index(1) do |num, i|
+    p "(i, num): (#{i}, #{num})"
+end
+
+# "(i, num): (1, 2)"
+# "(i, num): (2, 3)"
+
+nums[0...2].each_with_index do |num, i|
+    p "(i, num): (#{i}, #{num})"
+end
+
+# "(i, num): (0, 1)"
+# "(i, num): (1, 2)"
