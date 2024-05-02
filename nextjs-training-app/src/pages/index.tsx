e4162bf6a5a7dd1bcc6f6610d4fb6331bf141e09
TatsuroIde <Orust4t@gmail.com>
2024-05-02 18:19:34 +0900

    feat: add link to prev and next in home page

--- nextjs-training-app/src/pages/index.tsx
+++ nextjs-training-app/src/pages/index.tsx
@@ -1,10 +1,14 @@
 import MyServerComponent from "../components/api-data.server";
+import Link from "next/link";
 
 export default function Home({ data }: { data: any }) {
+  const id = data.id;
   return (
     <div>
       <h1>My Server Component</h1>
       <MyServerComponent data={data}/>
+      {id > 1 && <Link href={`/${id - 1}`}>Previous&nbsp;&nbsp;</Link>}
+      <Link href={`/${id + 1}`}>Next</Link>
     </div>
   );
 }
@@ -12,6 +16,5 @@ export default function Home({ data }: { data: any }) {
 export async function getServerSideProps() {
   const res = await fetch('https://pokeapi.co/api/v2/pokemon/pikachu');
   const data = await res.json();
-  console.log('aaa');
   return { props: { data } };
 }
